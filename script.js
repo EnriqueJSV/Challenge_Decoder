9030e274dbe28199792592254127057d0d886de1
Enrique Solorzano <ejsv1899@gmail.com>
2023-05-02 18:04:26 -0600

    Se agregan pequenos cambios de estilo al resultado y se agrega codigo js para ocultar y mostrar objetos

---
+++ script.js
@@ -0,0 +1,9 @@
+function ocultar(){
+    document.getElementById("b-copiar").style.visibility = 'hidden';
+}
+
+function mostrar(){
+    document.getElementById("b-copiar").style.visibility = 'visible';
+}
+
+ocultar();
