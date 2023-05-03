890bbabd4a5627cff0e992f7d617d7f93c78c70b
Enrique Solorzano <ejsv1899@gmail.com>
2023-05-02 18:18:32 -0600

    Se agregan las animaciones a los botones

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function ocultar(){
-    document.getElementById("b-copiar").style.visibility = 'hidden';
+let muneco = "img-muneco";
+let boton_copiar = "b-copiar";
+
+function ocultar(elemento){
+    document.getElementById(elemento).style.visibility = 'hidden';
 }
 
-function mostrar(){
-    document.getElementById("b-copiar").style.visibility = 'visible';
+function mostrar(elemento){
+    document.getElementById(elemento).style.visibility = 'visible';
 }
 
-ocultar();
